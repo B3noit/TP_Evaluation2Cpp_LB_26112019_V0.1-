a1ab49f9afae3a91b9f06d0cb00a9898a945ecb0
Ben <b.lamarre@xn--ludus-acadme-keb.com>
2019-11-26 18:50:15 +0100

    Exercice 69 énoncé a  (vrai main)
    En raison d'un petit problème avec VS voici le git avec le bon main qui s'appelle Vecteur3d.

--- Vecteur3d/Vecteur3d.cpp
+++ Vecteur3d/Vecteur3d.cpp
@@ -2,8 +2,17 @@
 //
 #include <iostream>
 #include <cstdlib>
+#include "cVecteur.h"
+
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+
+	cVecteur vecteur();//Création d'un vecteur avec le constructeur "par défaut"
+	cVecteur vecteur2(1,2,3); //Création d'un vecteur avec le deuxième constructeur avec paramètre
+
+	//Système pause
+	system("pause");
 }
